805161e73ffcfcbd35e4d82ebe2abf7749dc716c
Your Name <eliisa.heino@myy.haaga-helia.fi>
2023-02-27 18:28:11 +0200

    Valmis 27.2.2023

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, StatusBar, View } from 'react-native';
+import MapView, { Marker, pinColor, pin } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+ 
+  const [location, setLocation] = useState(null); // State where location is saved
+  const [pin, setPin] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+
+  });
+
+  useEffect(() => (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('No permission to get location')
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
+      setLocation(location);
+      console.log('Location:', location)
+
+    setPin({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+  });
+
+    })(), []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 60.200692,
+          longitude: 24.934302,
+          latitudeDelta: 0.0322,
+          longitudeDelta: 0.0221
+        }}
+        showsUserLocation={true}
+        onUserLocationChange={(e) => {
+          console.log("onUserLocationChange" , e.nativeEvent);
+        }}
+      >
+        <Marker
+          coordinate={pin}
+          pinColor='gold'
+          title='You are here'
+        />
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: StatusBar.currentHeight,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    flex: 1,
+    width: "100%",
+    height: "100%"
+  }
 });
